dd2fd2ddbb22e7b82275df3c1f1ebdd9027f96fd
sparksam <klutse.samuel@gmail.com>
2022-08-11 11:05:07 -0400

    Testing Github actions with Enki Docker image. Clone the Enki repository and build book.

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 echo 'Initializing Book Build'
-git clone https://github.com/openstax/enki
-cd enki
-ls -alt
 echo "./enki --command all-git-pdf --repo $INPUT_REPO_NAME --book-slug 'book-slug1' --style default"
 ./enki --command all-git-pdf --repo $INPUT_REPO_NAME --book-slug 'book-slug1' --style default
