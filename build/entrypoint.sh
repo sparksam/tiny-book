dc4e5ffd53e3b2ebd168f0497dc7b7321911e8db
sparksam <klutse.samuel@gmail.com>
2022-08-11 13:10:43 -0400

    Testing Github actions with Enki Docker image. Clone the Enki repository and build book. Test from entrypoint.sh

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -5,4 +5,4 @@ git clone https://github.com/openstax/enki
 cd enki
 ls -alt
 echo "IO_FETCHED=$INPUT_REPO_PATH START_AT=git-fetch-metadata dockerfiles/docker-entrypoint.sh all-git-epub "
-IO_FETCHED=$INPUT_REPO_PATH START_AT=git-fetch-metadata dockerfiles/docker-entrypoint.sh all-git-epub
+IO_FETCHED=$INPUT_REPO_PATH START_AT=git-fetch-metadata dockerfiles/docker-entrypoint.sh all-git-pdf
