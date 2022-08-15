94c251f387e33a1b7cfa812af96644e92b9dcaa8
sparksam <klutse.samuel@gmail.com>
2022-08-15 11:26:18 -0400

    Testing Github actions with Enki Docker image. Clone the Enki repository and build book. Test from entrypoint.sh

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -3,6 +3,23 @@
 echo 'Initializing Book Build'
 cd ../
 mkdir -p DATA_DIR/
+mkdir -p /github/DATA_DIR/IO_JSONIFIED
+mkdir -p /github/DATA_DIR/IO_DISASSEMBLE_LINKED
+mkdir -p /github/DATA_DIR/IO_DISASSEMBLED
+mkdir -p /github/DATA_DIR/IO_LINKED
+mkdir -p /github/DATA_DIR/IO_BAKE_META
+mkdir -p /github/DATA_DIR/IO_BAKED
+mkdir -p /github/DATA_DIR/IO_ASSEMBLE_META
+mkdir -p /github/DATA_DIR/IO_ASSEMBLED
+mkdir -p /github/DATA_DIR/IO_RESOURCES
+mkdir -p /github/DATA_DIR/IO_FETCH_META
+mkdir -p /github/DATA_DIR/IO_UNUSED_RESOURCES
+mkdir -p /github/DATA_DIR/IO_ARTIFACTS
+mkdir -p /github/DATA_DIR/IO_REX_LINKED
+mkdir -p /github/DATA_DIR/IO_MATHIFIED
+mkdir -p /github/DATA_DIR/IO_FETCHED
+mkdir -p /github/DATA_DIR/IO_BOOK
+mkdir -p /github/DATA_DIR/INPUT_SOURCE_DIR
 git clone https://github.com/openstax/enki
 cd enki
 #ls -alt
