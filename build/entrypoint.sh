e0153a9ea6c2d81c617e0d954fa6c123f568cc9a
sparksam <klutse.samuel@gmail.com>
2022-08-15 15:57:45 -0400

    Testing Github actions with Enki Docker image. Clone the Enki repository and build book. Test from entrypoint.sh

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -2,24 +2,24 @@
 
 echo 'Initializing Book Build'
 cd ../
-mkdir -p DATA_DIR/
-mkdir -p /github/DATA_DIR/IO_JSONIFIED
-mkdir -p /github/DATA_DIR/IO_DISASSEMBLE_LINKED
-mkdir -p /github/DATA_DIR/IO_DISASSEMBLED
-mkdir -p /github/DATA_DIR/IO_LINKED
-mkdir -p /github/DATA_DIR/IO_BAKE_META
-mkdir -p /github/DATA_DIR/IO_BAKED
-mkdir -p /github/DATA_DIR/IO_ASSEMBLE_META
-mkdir -p /github/DATA_DIR/IO_ASSEMBLED
-mkdir -p /github/DATA_DIR/IO_RESOURCES
-mkdir -p /github/DATA_DIR/IO_FETCH_META
-mkdir -p /github/DATA_DIR/IO_UNUSED_RESOURCES
-mkdir -p /github/DATA_DIR/IO_ARTIFACTS
-mkdir -p /github/DATA_DIR/IO_REX_LINKED
-mkdir -p /github/DATA_DIR/IO_MATHIFIED
-mkdir -p /github/DATA_DIR/IO_FETCHED
-mkdir -p /github/DATA_DIR/IO_BOOK
-mkdir -p /github/DATA_DIR/INPUT_SOURCE_DIR
+#mkdir -p DATA_DIR/
+#mkdir -p /github/DATA_DIR/IO_JSONIFIED
+#mkdir -p /github/DATA_DIR/IO_DISASSEMBLE_LINKED
+#mkdir -p /github/DATA_DIR/IO_DISASSEMBLED
+#mkdir -p /github/DATA_DIR/IO_LINKED
+#mkdir -p /github/DATA_DIR/IO_BAKE_META
+#mkdir -p /github/DATA_DIR/IO_BAKED
+#mkdir -p /github/DATA_DIR/IO_ASSEMBLE_META
+#mkdir -p /github/DATA_DIR/IO_ASSEMBLED
+#mkdir -p /github/DATA_DIR/IO_RESOURCES
+#mkdir -p /github/DATA_DIR/IO_FETCH_META
+#mkdir -p /github/DATA_DIR/IO_UNUSED_RESOURCES
+#mkdir -p /github/DATA_DIR/IO_ARTIFACTS
+#mkdir -p /github/DATA_DIR/IO_REX_LINKED
+#mkdir -p /github/DATA_DIR/IO_MATHIFIED
+#mkdir -p /github/DATA_DIR/IO_FETCHED
+#mkdir -p /github/DATA_DIR/IO_BOOK
+#mkdir -p /github/DATA_DIR/INPUT_SOURCE_DIR
 git clone https://github.com/openstax/enki
 cd enki
 #ls -alt
@@ -28,5 +28,4 @@ ls -alt $INPUT_REPO_PATH
 #ls -alt /opt
 #ls -alt /home
 #echo " TRACE_ON=1 IO_JSONIFIED=/github/DATA_DIR/IO_JSONIFIED  IO_DISASSEMBLE_LINKED=/github/DATA_DIR/IO_DISASSEMBLE_LINKED  IO_DISASSEMBLED=/github/DATA_DIR/IO_DISASSEMBLED  IO_LINKED=/github/DATA_DIR/IO_LINKED  IO_BAKE_META=/github/DATA_DIR/IO_BAKE_META  IO_BAKED=/github/DATA_DIR/IO_BAKED  IO_ASSEMBLE_META=/github/DATA_DIR/IO_ASSEMBLE_META  IO_ASSEMBLED=/github/DATA_DIR/IO_ASSEMBLED  IO_RESOURCES=/github/DATA_DIR/IO_RESOURCES  IO_FETCH_META=/github/DATA_DIR/IO_FETCH_META  IO_UNUSED_RESOURCES=/github/DATA_DIR/IO_UNUSED_RESOURCES  IO_ARTIFACTS=/github/DATA_DIR/IO_ARTIFACTS  IO_REX_LINKED=/github/DATA_DIR/IO_REX_LINKED  IO_MATHIFIED=/github/DATA_DIR/IO_MATHIFIED    IO_BOOK=/github/DATA_DIR/IO_BOOK  INPUT_SOURCE_DIR=/github/DATA_DIR/INPUT_SOURCE_DIR   IO_FETCHED=$INPUT_REPO_PATH START_AT_STEP=git-fetch-metadata dockerfiles/docker-entrypoint.sh all-git-pdf"
-TRACE_ON=1 IO_FETCHED=$INPUT_REPO_PATH START_AT_STEP=git-fetch-metadata dockerfiles/docker-entrypoint.sh local-create-book-directory
-TRACE_ON=1 IO_FETCHED=$INPUT_REPO_PATH START_AT_STEP=git-fetch-metadata dockerfiles/docker-entrypoint.sh all-git-pdf
+TRACE_ON=1  LOCAL_SIDELOAD_REPO_PATH=/github/workspace dockerfiles/docker-entrypoint.sh all-git-pdf --repo 'sparksam/tiny-book' --book-slug 'book-slug1' --ref main
